f7e8157a5dadcd90a60425ba896e459940ba36ae
stefano.boriero@gmail.com <stefano.boriero@gmail.com>
2023-04-12 13:03:28 +0200

    Add testing plugins and python runner

---
+++ lua/plugins/testing.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "vim-test/vim-test",
+  },
+  {
+    "nvim-neotest/neotest-vim-test",
+  },
+  {
+    "nvim-neotest/neotest-python",
+  },
+  {
+    "nvim-neotest/neotest",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "antoinemadec/FixCursorHold.nvim",
+    },
+  },
+}
