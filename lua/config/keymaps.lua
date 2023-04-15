3c466a2835873f4d3cfa30457b244ca5f13fa5e7
stefano.boriero@gmail.com <stefano.boriero@gmail.com>
2023-04-15 18:40:50 +0200

    Configure Neotest
    * Configure Neotest and key-mappings
    * Configure python test runner and debug adapter

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,7 +4,70 @@
 
 vim.keymap.set(
   "n",
-  "<leader>tn",
-  require("neotest").run.run(),
-  { noremap = true, silent = true, desc = "Test Nearest" }
+  "<leader>rtn",
+  '<cmd>:lua require("neotest").run.run()<cr>',
+  { noremap = true, silent = true, desc = "Run Test Nearest" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>rtf",
+  "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>",
+  { noremap = true, silent = true, desc = "Run Test File" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>rtl",
+  "<cmd>lua require('neotest').run.run_last()<cr>",
+  { noremap = true, silent = true, desc = "Run Test Last" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>dtn",
+  "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>",
+  { noremap = true, silent = true, desc = "Debug Test Nearest" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>dta",
+  "<cmd>lua require('neotest').run.attach()<cr>",
+  { noremap = true, silent = true, desc = "Debug Test Attach" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>dtf",
+  "<cmd>lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>",
+  { noremap = true, silent = true, desc = "Debug Test File" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>dtl",
+  "<cmd>lua require('neotest').run.run_last({ strategy = 'dap' })<cr>",
+  { noremap = true, silent = true, desc = "Debug Test Last" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>to",
+  "<cmd>lua require('neotest').output.open({ enter = true })<cr>",
+  { noremap = true, silent = true, desc = "Test Output" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>ts",
+  "<cmd>lua require('neotest').summary.toggle()<cr>",
+  { noremap = true, silent = true, desc = "Test Summary" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>tS",
+  "<cmd>lua require('neotest').run.stop()<cr>",
+  { noremap = true, silent = true, desc = "Test Stop" }
 )
--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -3,17 +3,33 @@ return {
     "vim-test/vim-test",
   },
   {
-    "nvim-neotest/neotest-vim-test",
-  },
-  {
     "nvim-neotest/neotest-python",
   },
   {
     "nvim-neotest/neotest",
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-python")({
+            dap = { justMyCode = false },
+          }),
+        },
+      })
+    end,
+    lazy = false,
     requires = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
       "antoinemadec/FixCursorHold.nvim",
     },
+  },
+  {
+    "mfussenegger/nvim-dap",
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    config = function()
+      require("dap-python").setup()
+    end,
   },
 }
